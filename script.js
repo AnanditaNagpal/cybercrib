9dfd3caaed5dd01dabc7922f8fee361633e0f286
ANANDITA NAGPAL <143868143+AnanditaNagpal@users.noreply.github.com>
2025-04-18 16:03:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+
+// Toggle dark mode
+document.addEventListener('DOMContentLoaded', function () {
+  const toggle = document.getElementById('theme-toggle');
+  toggle.addEventListener('change', () => {
+    document.body.classList.toggle('dark-mode');
+  });
+});
